6cd17d5f2c9a14f1df5fb1dc56095fc9ece8dc37
Josh Lindsay <jlindsay90@gmail.com>
2018-03-26 23:39:05 -0700

    added simple sqlalchemy to store move data

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 import os
 import wwf_client as WWF
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import psycopg2
+from sqlalchemy.dialects.postgresql import ARRAY
 
 """
 TODO
@@ -18,6 +21,28 @@ http://flask.pocoo.org/docs/0.12/patterns/sqlalchemy/
 
 TITLE = 'Words With Fiends'
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wwfiends'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Move(db.Model):
+    __tablename__ = "moves"
+    id = db.Column(db.BigInteger, primary_key=True)
+    game_id = db.Column(db.BigInteger)
+    user_id = db.Column(db.BigInteger)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    move_type = db.Column(db.String(80))
+    from_x = db.Column(db.Integer)
+    from_y = db.Column(db.Integer)
+    to_x = db.Column(db.Integer)
+    to_y = db.Column(db.Integer)
+    move_index = db.Column(db.Integer)
+    text = db.Column(db.String(80))
+    board_checksum = db.Column(db.BigInteger)
+    points = db.Column(db.Integer)
+    promoted = db.Column(db.Integer)
+    words = db.ARRAY(db.String(80))
+    data = None
 
 
 @app.route('/')
@@ -41,6 +66,22 @@ def play():
     games = WWF.get_games(s)
     for g in games:
         (board, st) = WWF.get_nums(g['moves'])
+
+
+@app.cli.command()
+def store():
+    s = WWF.login(*get_credentials())
+    games = WWF.get_games(s)
+    for g in games:
+        moves = g['moves']
+        for m in moves:
+            move = Move(**m)
+            try:
+                db.session.add(move)
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                db.session.rollback()
 
 
 @app.cli.command()
