d14cff629226d8daaab19c040e163b721b8262fc
Josh Lindsay <jlindsay90@gmail.com>
2018-04-10 01:55:31 -0700

    moved db models to own class, store games moves users

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 import os
 import wwf_client as WWF
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import psycopg2
-from sqlalchemy.dialects.postgresql import ARRAY
+from database import db, Move, Game, User, Word
+import json
+import gzip
 
 """
 TODO
@@ -23,41 +22,18 @@ Djangoify!
 TITLE = 'Words With Fiends'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-class Move(db.Model):
-    __tablename__ = "moves"
-    id = db.Column(db.BigInteger, primary_key=True)
-    game_id = db.Column(db.BigInteger)
-    user_id = db.Column(db.BigInteger)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    move_type = db.Column(db.String(80))
-    from_x = db.Column(db.Integer)
-    from_y = db.Column(db.Integer)
-    to_x = db.Column(db.Integer)
-    to_y = db.Column(db.Integer)
-    move_index = db.Column(db.Integer)
-    text = db.Column(db.String(80))
-    board_checksum = db.Column(db.BigInteger)
-    points = db.Column(db.Integer)
-    promoted = db.Column(db.Integer)
-    words = db.ARRAY(db.String(80))
-    data = None
+db.init_app(app)
 
 @app.route('/')
-def home():
-    return test()
-
-@app.route('/test')
-def test():
+def show():
     disp = ""
     s = WWF.login(*get_credentials())
     games = WWF.get_games(s)
     link = '<a href="https://cs.rit.edu/~jal3040/files/fiends.html">more</a>'
     for g in games:
-        (board, st) = WWF.get_nums(g['moves'])
+        (_, st) = WWF.get_nums(g['moves'])
         disp += f'\n{st}\n'
     return f'<!DOCTYPE html>\n<html><body><pre>\n{disp}\n</pre>{link}</body></html>'
 
@@ -67,28 +43,64 @@ def play():
     games = WWF.get_games(s)
     for g in games:
         (board, st) = WWF.get_nums(g['moves'])
+        pass
 
 @app.cli.command()
 def work():
     s = WWF.login(*get_credentials())
     r = WWF.get_daily_drip(s)
     print(r.json())
+
+@app.cli.command()
+def store():
+    s = WWF.login(*get_credentials())
     games = WWF.get_games(s)
-
-    # store moves in db
     for g in games:
-        moves = g['moves']
-        for m in moves:
-            move = Move(**m)
-            try:
-                db.session.add(move)
-                db.session.commit()
-            except Exception as e:
-                db.session.rollback()
+        store_game(g)
 
-def get_credentials():
-    username = os.environ.get('WWF_USER')
-    password = os.environ.get('WWF_PASS')
+def store_game(game):
+    store_users(game)
+    store_moves(game['moves'])
+
+    try:
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+
+def store_users(game):
+    for user in game['users']:
+        u = User(**user)
+        try:
+            db.session.add(u)
+        except Exception:
+            db.session.rollback()
+
+def store_moves(moves):
+    for move in moves:
+        move['word'] = move['words'][0]
+        m = Move(**m)
+        try:
+            db.session.add(m)
+        except Exception:
+            db.session.rollback()
+
+@app.cli.command()
+def words():
+    words = json.load(gzip.open('words.txt.gz', 'rb'))
+    for w in words:
+        try:
+            word = Word(w)
+            db.session.add(word)
+        except Exception as e:
+            db.session.rollback()
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+
+def get_credentials(user_env = 'WWF_USER', pswd_env = 'WWF_PASS'):
+    username = os.environ.get(user_env)
+    password = os.environ.get(pswd_env)
     return (username, password)
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import ARRAY
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+
+db = SQLAlchemy()
+
+class Move(db.Model):
+    id = db.Column(db.BigInteger, primary_key = True)
+    game_id = db.Column(db.BigInteger, db.ForeignKey('game.id'))
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    move_type = db.Column(db.String(80))
+    from_x = db.Column(db.Integer)
+    from_y = db.Column(db.Integer)
+    to_x = db.Column(db.Integer)
+    to_y = db.Column(db.Integer)
+    move_index = db.Column(db.Integer)
+    text = db.Column(db.String(80))
+    board_checksum = db.Column(db.BigInteger)
+    points = db.Column(db.Integer)
+    promoted = db.Column(db.Integer)
+    word = db.Column(db.String(80))
+    words = db.Column(db.ARRAY(db.String(80)))
+    data = None
+
+class Game(db.Model):
+    id = db.Column(db.BigInteger, primary_key = True)
+    client_version = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    word = db.Column(db.String(80))
+    current_move_user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
+    created_by_user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'))
+    is_fsn = db.Column(db.Boolean)
+    is_matchmaking = db.Column(db.Boolean)
+    was_matchmaking = db.Column(db.Boolean)
+    move_count = db.Column(db.Integer)
+    random_seed = db.Column(db.BigInteger)
+    create_type = db.Column(db.String(80))
+
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key = True)
+    name = db.Column(db.String(80))
+    zynga_account_id = db.Column(db.BigInteger)
+
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    word = db.Column(db.String(80))
